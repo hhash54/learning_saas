37b9c6235cff45a9db3501edfe793f77bb839a19
hhash54 <hhashem2002@gmail.com>
2025-05-15 15:14:25 -0400

    nondashboar page

--- src/components/NonDashboardNavbar.tsx
+++ src/components/NonDashboardNavbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
-
+import { Bell, Book, BookOpen } from "lucide-react";
 const NonDashboardNavbar = () => {
   return (
     <nav className="nondashboard-navbar">
       <div className="nondashboard-navbar__container">
+        <div className="nondashboard-navbar__search">
         <Link href="/" className="nondashboard-navbar__brand">
           EDROH
         </Link>
@@ -14,8 +15,17 @@ const NonDashboardNavbar = () => {
               <span className="hidden sm:inline">Search Courses</span>
               <span className="sm:hidden">Search</span>
             </Link>
+            <BookOpen className="nondashboard-navbar__search-icon" size={18}/>
           </div>
         </div>
+        </div>
+        
+      </div>
+      <div className="nondashboard-navbar__actions">
+        <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span> 
+            <Bell className="nondashboard-navbar__notification-icon"/>
+        </button>
       </div>
     </nav>
   );
