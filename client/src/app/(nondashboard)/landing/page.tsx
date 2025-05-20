0141388e772a263d4f625e3616b2c8fb533e44dc
hhash54 <hhashem2002@gmail.com>
2025-05-20 19:44:57 -0400

    create api endpoint for front-end and back-end

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useCarousel } from "@/hooks/useCarousel";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useGetCoursesQuery } from "@/state/api";
 const LoadingSkeleton = () => {
   return (
     <div className="landing-skeleton">
@@ -39,6 +40,9 @@ const LoadingSkeleton = () => {
 
 const Landing = () => {
     const currentImage=useCarousel({totalImages:3});
+    const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+    console.log("courses:", courses);
+
     return (
     <motion.div
       initial={{ opacity: 0 }}
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -3,8 +3,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  tagTypes: ["Courses"],
+  endpoints: (build) => ({
+    getCourses: build.query<Course[], { category?: string }>({
+      query: ({ category }) => ({
+        url: "courses",
+        params: { category },
+      }),
+      providesTags: ["Courses"],
+    }),
+    getCourse: build.query<Course, string>({
+      query: (id) => `courses/${id}`,
+      providesTags: (result, error, id) => [{ type: "Courses", id }],
+    }),
+  }),
 });
 
-export const {} = api;
+export const {useGetCoursesQuery, useGetCourseQuery} = api;
