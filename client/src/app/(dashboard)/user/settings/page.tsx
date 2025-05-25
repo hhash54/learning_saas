bedb567fab630feb4d643e3325e3b2d157c16160
hhash54 <hhashem2002@gmail.com>
2025-05-25 12:04:41 -0400

    setup  settings page pt2

--- client/src/app/(dashboard)/user/settings/page.tsx
+++ client/src/app/(dashboard)/user/settings/page.tsx
@@ -1,9 +1,13 @@
+import SharedNotificationSettings from "@/components/SharedNotificationSettings";
 import React from "react";
 
 const UserSettings = () => {
   return (
     <div className="w-3/5">
-      <SharedNotificationSettings />
+      <SharedNotificationSettings
+        title="User Settings"
+        subtitle="Manage your user notification settings"
+      />
     </div>
   );
 };
