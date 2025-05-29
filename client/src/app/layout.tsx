edbfdbafb91121c92e1f24e2e1518b60f9832d5b
hhash54 <hhashem2002@gmail.com>
2025-05-28 23:44:22 -0400

    api stuff

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import {ClerkProvider} from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
 const dmSans = DM_Sans({
   subsets: ["latin"],
   display: "swap",
@@ -28,7 +30,10 @@ export default function RootLayout({
           <div className="root-layout">
             {children}
           </div>
-        </Providers>        
+          <Toaster richColors closeButton />
+
+        </Providers> 
+
       </body>
     </html>
     </ClerkProvider>
