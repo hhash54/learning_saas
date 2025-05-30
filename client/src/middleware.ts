d6826c1a43ca2d2672fae34c599f1f8d2d830db9
hhash54 <hhashem2002@gmail.com>
2025-05-30 16:52:26 -0400

    teacher courses path

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,7 +6,6 @@ const isTeacherRoute = createRouteMatcher(["/teacher/(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { sessionClaims } = await auth();
-  
   const userRole =
     (sessionClaims?.metadata as { userType: "student" | "teacher" })
       ?.userType || "student";
